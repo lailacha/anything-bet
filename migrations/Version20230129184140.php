de6d4f6a28a2674ca46d8e787e45b37eed1cac31
LailaCharaoui <mats2@live.fr>
2023-01-30 00:30:45 +0100

    init project

---
+++ migrations/Version20230129184140.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230129184140 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX uniq_8d93d6495f37a13b');
+        $this->addSql('ALTER TABLE "user" DROP token');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" ADD token VARCHAR(180) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX uniq_8d93d6495f37a13b ON "user" (token)');
+    }
+}
